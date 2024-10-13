dfca9b31ed96b548c117180cde25b272be8954e7
38tter <odradek38@gmail.com>
2024-10-13 17:45:24 +0900

    Fix main.go

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 )
 
-func main() {
+func Upsert(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w)
+
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	path := os.Getenv("INSTANCE_UNIX_SOCKET")
@@ -26,6 +29,17 @@ func main() {
 	if err != nil {
 		log.Fatalf("SQL ERROR: %s", err)
 	}
+}
 
-	return
+func main() {
+	http.HandleFunc("/", Upsert)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
